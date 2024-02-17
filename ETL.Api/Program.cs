1e0ddaf5ec27ead489144d7ae669ae448bc26350
Maxim <mxm1337@rambler.ru>
2024-02-17 21:44:13 +0300

    dependencies configuration order

--- ETL.Api/Program.cs
+++ ETL.Api/Program.cs
@@ -8,16 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
 builder.Services.AddDbContext<UniversityDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("NpgUniversitiesDbConnection"));
@@ -36,6 +26,16 @@ builder.Services.AddHttpClient<IExtractor, Extractor>(client =>
 });
 builder.Services.AddSingleton(externalApiConfig);
 builder.Services.AddSingleton<IExtractor, Extractor>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 app.MapGet("/universities", async (
         [FromKeyedServices("pg_repo")] IUniversityRepository universityRepository,
